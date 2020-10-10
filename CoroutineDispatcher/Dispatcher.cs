5d0260c3c4e9fc468d040bae592e4e900da96a57
Tomasz Rewak <tomaszrewak@gmail.com>
2020-10-10 16:01:37 +0200

    Add project files.

---
+++ CoroutineDispatcher/Dispatcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CoroutineDispatcher
+{
+	public class Dispatcher
+	{
+		public void Start()
+		{
+
+		}
+
+		public static void Spawn()
+		{
+
+		}
+
+		public void Invoke()
+		{
+
+		}
+
+		//public T Invoke<T>()
+		//{
+
+		//}
+
+		public void InvokeAsync()
+		{
+
+		}
+
+		//public Task<T> InvokeAsync<T>()
+		//{
+
+		//}
+
+		public void Dispatch()
+		{
+
+		}
+	}
+}
---
+++ CoroutineDispatcher/TaskQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoroutineDispatcher
+{
+	internal sealed class TaskQueue
+	{
+		private readonly Queue<Action>[] _pendingTasks = new Queue<Action>[]
+		{
+			new Queue<Action>(),
+			new Queue<Action>(),
+			new Queue<Action>()
+		};
+
+		public void Add(Action action, DispatchPriority priority)
+		{
+			_pendingTasks[(int)priority].Enqueue(action);
+		}
+	}
+}
